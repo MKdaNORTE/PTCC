465058dab0bf4973dfffe7a34ed9d776ae58cd7e
MKdaNORTE <marcussnts05@gmail.com>
2025-03-21 09:37:29 -0300

    modified:   conexao.php

--- conexao.php
+++ conexao.php
@@ -4,7 +4,7 @@ $db_nome = 'panificadora_db'; // nome do banco de dados
 $db_usuario = 'root'; // usuário do banco de dados
 $db_senha = ''; // senha do banco de dados
 
-$conexao = new mysqli($db_host, $db_usuario, $db_senha, $db_nome);
+$conexao = ew mysqli($db_host, $db_usuario, $db_senha, $db_nome);
 
 if ($conexao->connect_error) {
     die ("Erro de conexão: " . $conexao->connect_error);
